bae0623c137d8b68426409adea84e3a5dfa5e49b
LinQuan <i@linquan.name>
2016-11-09 13:21:02 +0800

    second commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+var resolve = require('app-root-path').resolve;
+var objectAssign = require('object-assign');
+
+function loadAllConfig(pth, env, opts) {
+  if (typeof pth === 'string') {
+    pth = {
+      path: resolve(pth)
+    };
+  }
+
+  if (typeof env === 'string') {
+    env = {
+      env: env
+    };
+  }
+
+  opts = objectAssign({
+    path: resolve('./config'),
+    env_path: 'env',
+    env: 'development'
+  }, opts, env, pth);
+
+  var env_config_pth = opts.path + '/' + opts.env_path + '/' + opts.env + '.js';
+  var common_config_pth = opts.path + '/config.js';
+  var configs = {};
+  try{
+    var env_config = require(env_config_pth);
+    objectAssign(configs, env_config);
+  } catch(e) {};
+  try{
+    common_config = require(common_config_pth);
+    objectAssign(configs, common_config);
+  } catch(e) {};
+  var congif_files = fs.readdirSync(opts.path);
+  congif_files.forEach(function(cfg_file, i) {
+      if(cfg_file == 'config.js') {
+        return;
+      }
+      var cfg_path = opts.path + '/' + cfg_file;
+      var cfg_key = cfg_file.replace(/\.js$/, '');
+      var cfg_obj = {};
+      cfg_obj[cfg_key] = require(cfg_path);
+      objectAssign(configs, cfg_obj);
+  });
+  return configs;
+}
+
+module.exports = loadAllConfig;
